e8e83141c2085127e2e21f1674c9556c13508ca2
uchchhwash <uchchhwash.git@gmail.com>
2020-05-29 11:56:30 +0600

    requirement.txt , config & app init file update

---
+++ app/__init__.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# from app.models import actor, movie
+# from app.routes import index, actors, movies
+
+# Error Handling
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Not Found."
+    }), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({
+        "success": False,
+        "error": 405,
+        "message": error.description
+    }), 405
+
+
+@app.errorhandler(401)
+def permissions_error(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "Unauthorized! Permissions error."
+    }), 401
+
+
+@app.errorhandler(400)
+def user_error(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": error.description
+    }), 400
+
+
+@app.errorhandler(401)
+def invalid_claims(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": error.__dict__
+    }), 401
