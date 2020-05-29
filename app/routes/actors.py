1fbe03f2aa7d3cdd57e7893dffa2b4c2f2841bde
uchchhwash <uchchhwash.git@gmail.com>
2020-05-29 12:56:06 +0600

    api post /actors created

--- app/routes/actors.py
+++ app/routes/actors.py
@@ -19,3 +19,25 @@ def get_actors(payload):
         'actors': actors,
         'success': True
     }), 200
+
+@app.route('/actors', methods=['POST'])
+@requires_auth('post:actors')
+def add_actor(payload):
+    """Handles POST requests for actors.
+    returns:
+        - actor object
+        - success message
+    """
+    body = request.get_json()
+    new_actor = Actor(
+        name=body.get('name'),
+        bio=body.get('bio'),
+        age=body.get('age'),
+        gender=body.get('gender'),
+        movie=body.get('movie')
+    )
+    new_actor.save()
+    return jsonify({
+        'actor': new_actor.to_json(),
+        'success': True
+    }), 201
