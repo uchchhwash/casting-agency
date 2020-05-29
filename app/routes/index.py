4bbef10fcd0bc78b2a56f4df653bc145042fe6a2
uchchhwash <uchchhwash.git@gmail.com>
2020-05-29 12:53:52 +0600

    index route intialization

---
+++ app/routes/index.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+from app import app
+
+
+@app.route('/')
+def index():
+    """Handles GET requests for sample ping.
+    This is used to test whether the app is set up well.
+    returns:
+        - success message
+    """
+    return jsonify({
+        'message': 'Udacity capstone project.',
+        'success': True
+    })
