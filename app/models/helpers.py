ecea6eeab616e84143ab71fb7f245b40d2479f84
uchchhwash <uchchhwash.git@gmail.com>
2020-05-29 12:01:12 +0600

    model helper initialization

---
+++ app/models/helpers.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+class ModelHelpers():
+    """Model helper methods."""
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
